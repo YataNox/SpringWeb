af0ec44df198d5e232aa7d3dab33bac4549e8891
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-12-13 22:13:36 +0900

    Dto, dao Create

---
+++ SpringWeb03_ShoppingMall/src/main/java/com/ezen/shop/dao/ProductDao.java
@@ -0,0 +1,9 @@
+package com.ezen.shop.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductDao {
+	private JdbcTemplate template;
+}
