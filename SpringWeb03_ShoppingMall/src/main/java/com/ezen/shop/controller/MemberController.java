9f68feeef6d76c83c8c4818673649589611df683
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-12-14 16:29:39 +0900

    Controller, Service, Dao connection

--- SpringWeb03_ShoppingMall/src/main/java/com/ezen/shop/controller/MemberController.java
+++ SpringWeb03_ShoppingMall/src/main/java/com/ezen/shop/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.ezen.shop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.ezen.shop.service.MemberService;
 
 @Controller
 public class MemberController {
-	
+	@Autowired
+	MemberService ms;
 }
--- SpringWeb03_ShoppingMall/src/main/java/com/ezen/shop/dao/ProductDao.java
+++ SpringWeb03_ShoppingMall/src/main/java/com/ezen/shop/dao/ProductDao.java
@@ -15,13 +15,19 @@ public class ProductDao {
 	// template에 초기화해줘야 합니다.
 	
 	// 스프링 컨테이너에 수동으로 넣어놓은 DBCP 클래스를 꺼내옵니다.
-	@Autowired
-	ComboPooledDataSource dataSource;
+	// @Autowired
+	// ComboPooledDataSource dataSource;
 	
 	// 생성자 메소드와 꺼내온 DBCP 빈을 이용하여 탬플릿을 초기화합니다.
-	public ProductDao(){
+	// public ProductDao(){
+	// 	this.template = new JdbcTemplate(dataSource);
+	// }
+	
+	@Autowired // Autowired로 빈을 꺼내와서 담는 동작은 생성자의 전달인수에도 가능합니다.
+	public ProductDao(ComboPooledDataSource dataSource) {
 		this.template = new JdbcTemplate(dataSource);
 	}
+	// 스프링 컨테이너의 dataSource -> 생성자의 전달인수로 -> jdbcTemplate의 생성자의 전달인수로..
 	
 	// 이로써 연결을 이용한 데이터베이스 운용이 가능해졌습니다.
 }
