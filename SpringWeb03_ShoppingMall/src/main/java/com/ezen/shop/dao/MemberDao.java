edcc8a0d7fb778a9733bc6eda74e0bdf7a359cf9
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-12-14 18:43:16 +0900

    getMember Method Coment Add

--- SpringWeb03_ShoppingMall/src/main/java/com/ezen/shop/dao/MemberDao.java
+++ SpringWeb03_ShoppingMall/src/main/java/com/ezen/shop/dao/MemberDao.java
@@ -48,5 +48,15 @@ private JdbcTemplate template;
 		}else {
 			return list.get(0);
 		}
+		
+		// 1. template.query의 첫 번째 전달인수로 sql을 위치시킵니다.
+		// 2. 두 번째 전달인수로 new RowMapper<>(){}를 위치시킵니다.
+		// 3. 이때 <>안에는 리턴형 자료의 Dto 이름을 써줍니다.
+		// 4. 두 번째 전달인수로 들어간 RowMapper 생성자를 이용하여 Override될 메소드를 추가합ㄴ디ㅏ.
+		// 5. 추가된 메소드에 Dto 객체를 생성하고, 각 멤버변수를 rs.getInt or rs.getString등을 이용하여 set합니다.
+		// 6. 다 채워진 Dto 객체를 리턴합니다.
+		// 7. 마지막 괄호 앞에 (,)로 구분하여 ?에 들어갈 인수를 차례로 써줍니다.
+		// 8. List가 아닌 단일 Dto로 리턴되어야 한다면 list.get(0)을 리턴합니다.
+		// 		단 리턴 값이 null일 경우 if로 처리합니다.
 	}
 }
