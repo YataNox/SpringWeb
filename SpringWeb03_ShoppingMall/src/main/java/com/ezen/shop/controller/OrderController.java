25664ec167275a56020e0760c33fd98236912690
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-12-15 20:32:26 +0900

    orderList Part Write

--- SpringWeb03_ShoppingMall/src/main/java/com/ezen/shop/controller/OrderController.java
+++ SpringWeb03_ShoppingMall/src/main/java/com/ezen/shop/controller/OrderController.java
@@ -1,12 +1,67 @@
 package com.ezen.shop.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.ezen.shop.dto.CartVO;
+import com.ezen.shop.dto.MemberVO;
+import com.ezen.shop.dto.OrderVO;
+import com.ezen.shop.service.CartService;
 import com.ezen.shop.service.OrderService;
 
 @Controller
 public class OrderController {
 	@Autowired
 	OrderService os;
+	
+	@Autowired
+	CartService cs;
+	
+	@RequestMapping(value="/orderInsert")
+	public String orderInsert(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		MemberVO mvo = (MemberVO)session.getAttribute("loginUser");
+		int oseq = 0;
+		if(mvo == null) {
+			return "member/login";
+		}else {
+			// 현재 로그인 중인 사용자의 아이디로 카트리스트 조회
+			List<CartVO> cartList = cs.listCart(mvo.getId());
+			
+			// 카트 리스트를 주문으로 추가
+			oseq = os.insertOrder(cartList, mvo.getId());
+			
+			// 방금 추가한 주문 번호를 리턴받아 오더 리스트의 주문번호로 적용합니다..
+		}
+		return "redirect:/orderList?oseq=" + oseq;
+	}
+	
+	@RequestMapping(value="/orderList")
+	public ModelAndView orderList(HttpServletRequest request, @RequestParam("oseq") int oseq) {
+		ModelAndView mav = new ModelAndView();
+		HttpSession session = request.getSession();
+		MemberVO mvo = (MemberVO)session.getAttribute("loginUser");
+		if(mvo == null) {
+			mav.setViewName("member/login");
+		}else {
+			List<OrderVO> list = os.listOrderByOseq(oseq);
+			 int totalPrice=0;
+	         for(OrderVO ovo : list)
+	            totalPrice += ovo.getPrice2() * ovo.getQuantity();
+	         mav.addObject("orderList", list);
+	         mav.addObject("totalPrice", totalPrice);
+	         mav.setViewName("mypage/orderList");
+
+		}
+		
+		return mav;
+	}
 }
--- SpringWeb03_ShoppingMall/src/main/java/com/ezen/shop/dao/OrderDao.java
+++ SpringWeb03_ShoppingMall/src/main/java/com/ezen/shop/dao/OrderDao.java
@@ -1,9 +1,16 @@
 package com.ezen.shop.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import com.ezen.shop.dto.CartVO;
+import com.ezen.shop.dto.OrderVO;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Repository
@@ -14,4 +21,64 @@ private JdbcTemplate template;
 	public OrderDao(ComboPooledDataSource dataSource) {
 		this.template = new JdbcTemplate(dataSource);
 	}
+
+	public int insertOrder(String id) {
+		String sql ="insert into orders(oseq, id) values(orders_seq.nextVal, ?)";
+		
+		template.update(sql, id);
+		
+		return 0;
+	}
+
+	public int LookupMaxOseq() {
+		String sql = "select max(oseq) as moseq from orders";
+		
+		List<Integer> list = template.query(sql, new RowMapper<Integer>() {
+			@Override
+			public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				int maxoseq = rs.getInt("moseq");
+				return maxoseq;
+			}
+		});
+		
+		return list.get(0);
+	}
+
+	public void insertOrderDetail(CartVO cvo, int oseq) {
+		String sql = "insert into order_detail(odseq, oseq, pseq, quantity) "
+				+ "values(order_detail_seq.nextVal, ?, ?, ?)";
+		
+		template.update(sql, oseq, cvo.getPseq(), cvo.getQuantity());		
+	}
+
+	public void deleteCart(Integer cseq) {
+		String sql ="delete from cart where cseq = ?";
+		
+		template.update(sql, cseq);
+	}
+
+	public List<OrderVO> listOrderByOseq(int oseq) {
+	      String sql = "select * from order_view where oseq=?";
+	      List<OrderVO> list = template.query(sql, new RowMapper<OrderVO>() {
+	         @Override
+	         public OrderVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+	            OrderVO ovo = new OrderVO();
+	            ovo.setOdseq(rs.getInt("odseq"));
+	            ovo.setOseq(rs.getInt("oseq"));
+	            ovo.setId(rs.getString("id"));
+	            ovo.setIndate(rs.getTimestamp("indate"));
+	            ovo.setMname(rs.getString("mname"));
+	            ovo.setZip_num(rs.getString("zip_num"));
+	            ovo.setAddress(rs.getString("address"));
+	            ovo.setPhone(rs.getString("phone"));
+	            ovo.setPseq(rs.getInt("pseq"));
+	            ovo.setQuantity(rs.getInt("quantity"));
+	            ovo.setPname(rs.getString("pname"));
+	            ovo.setPrice2(rs.getInt("price2"));
+	            ovo.setResult(rs.getString("result"));
+	            return ovo;
+	         }
+	      }, oseq);
+	      return list;
+	   }
 }
